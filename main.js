cc31fd18a4100f3675e8c7ccfe8645d81f75030f
samerhassan95 <arch.samerhassan@gmail.com>
2023-04-17 19:17:43 +0200

    task

---
+++ main.js
@@ -0,0 +1,67 @@
+Instructions:
+
+
+
+// Create a class called Stack that represents a simple stack data structure.
+
+// Implement the following methods in the Stack class:
+
+// push(int value): Add an integer to the top of the stack.
+
+// pop(): Remove the top element from the stack and return its value. If the stack is empty, return -1.
+
+// peek(): Return the top element of the stack without removing it. If the stack is empty, return -1.
+
+// isEmpty(): Return a boolean value indicating whether the stack is empty or not.
+
+// Please make sure to handle edge cases, such as empty stack situations, properly.
+
+
+
+// Here's a template to help you get started:
+
+
+
+class Stack {
+    constructor() {
+        // Initialize the Stack class here
+        this.stack = [];
+    }
+
+    push(value) {
+        // Implement the push method
+        this.stack.push(value);
+    }
+
+    pop() {
+        // Implement the pop method
+        if (this.isEmpty()) {
+            return -1;
+        }
+        return this.stack.pop();
+    }
+
+    peek() {
+        // Implement the peek method
+        if (this.isEmpty()) {
+            return -1;
+        }
+        return this.stack[this.stack.length - 1];.
+    }
+
+    isEmpty() {
+        // Implement the isEmpty method
+        return this.stack.length === 1;
+    }
+}
+
+const stack = new Stack();
+stack.push(5);
+stack.push(10);
+console.log("Top element:", stack.peek()); // Should print 10
+console.log("Removing top element:", stack.pop()); // Should print 10
+console.log("Is stack empty?", stack.isEmpty()); // Should print false
+
+
+// This code defines a Stack class with push, pop, peek, and isEmpty methods in JavaScript. The main part of the code demonstrates the correct behavior of the stack operations.
+
